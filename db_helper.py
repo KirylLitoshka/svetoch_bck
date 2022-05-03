77db068884f0bfb3064fb2553d6f52d564bda993
Kiryl Litoshka <kovzik08@gmail.com>
2022-05-03 18:45:12 +0300

    added test db initialization

---
+++ db_helper.py
@@ -0,0 +1,35 @@
+import asyncio
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from app.settings import CONFIG_DIR
+from app.utils import get_config, construct_db_url
+from app.models import Base, Subsystem
+
+test_config = get_config(CONFIG_DIR)
+DB_URL = construct_db_url(test_config["postgres"])
+
+
+async def init_db():
+    engine = create_async_engine(DB_URL)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+    async with session() as session:
+        async with session.begin():
+            session.add_all([
+                Subsystem(name="accounting", alt_name="Бухгалтерия"),
+                Subsystem(name="energy", alt_name="Электроэнергия"),
+                Subsystem(name="water", alt_name="Вода"),
+                Subsystem(name="tbo", alt_name="ТБО"),
+            ])
+
+    await engine.dispose()
+
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
