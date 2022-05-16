735195156000845894e364a502382d4c4be26b77
Kiryl Litoshka <kovzik08@gmail.com>
2022-05-16 15:42:49 +0300

    added swagger for api doc

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from aiohttp import web
-
+from aiohttp_swagger import setup_swagger
 from app.main import init_app
 from app.utils import get_config
 from app.settings import CONFIG_DIR
@@ -8,6 +8,7 @@ from app.settings import CONFIG_DIR
 def main():
     config = get_config(CONFIG_DIR)
     app = init_app(config)
+    setup_swagger(app, swagger_url="api/v1/doc")
     web.run_app(app, host=config["host"], port=config["port"])
 
 
