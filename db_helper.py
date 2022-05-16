4c040a78e74fe0f3289362ac20b2ef59e4fb3f13
Kiryl Litoshka <kovzik08@gmail.com>
2022-05-16 15:46:49 +0300

    updated db helper (objects files will be added after tests)

--- db_helper.py
+++ db_helper.py
@@ -1,13 +1,36 @@
 import asyncio
+import json
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.settings import CONFIG_DIR
 from app.utils import get_config, construct_db_url
-from app.models import Base, Subsystem
+from app.models import Base, Subsystem, Service
 
 test_config = get_config(CONFIG_DIR)
 DB_URL = construct_db_url(test_config["postgres"])
+
+
+async def create_subsystem_data(session):
+    async with session.begin():
+        with open("test_data/sybsystems.json") as data_file:
+            subsystem_list = json.load(data_file)
+        if not subsystem_list:
+            raise
+
+        for subsystem in subsystem_list:
+            session.add(Subsystem(**subsystem))
+
+
+async def create_services_data(session):
+    async with session.begin():
+        with open("test_data/services.json") as data_file:
+            service_list = json.load(data_file)
+        if not service_list:
+            raise
+
+        for service in service_list:
+            session.add(Service(**service))
 
 
 async def init_db():
@@ -20,13 +43,8 @@ async def init_db():
     session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
     async with session() as session:
-        async with session.begin():
-            session.add_all([
-                Subsystem(name="accounting", alt_name="Бухгалтерия"),
-                Subsystem(name="energy", alt_name="Электроэнергия"),
-                Subsystem(name="water", alt_name="Вода"),
-                Subsystem(name="tbo", alt_name="ТБО"),
-            ])
+        await create_subsystem_data(session)
+        await create_services_data(session)
 
     await engine.dispose()
 
