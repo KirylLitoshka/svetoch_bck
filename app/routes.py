9af83b788f6f597a6eaf4e75f02f5792bd3fc89f
Kiryl Litoshka <kovzik08@gmail.com>
2022-05-16 15:45:23 +0300

    updated routes after adding service model

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,6 @@ from .views import *
 
 routes = [
     web.view("/api/v1/subsystems", SubsystemListView),
+    web.view("/api/v1/subsystems/{name}", SubsystemDetailView),
+    web.view("/api/v1/subsystems/{name}/menu", ServicesListView)
 ]
